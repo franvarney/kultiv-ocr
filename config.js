c18e8ed626d844c47f4bc30303c051d45b831214
Fran Varney <fran.varn@gmail.com>
2017-02-24 09:25:11 -0500

    Add log level config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  logLevel: process.env.LOG_LEVEL || 'info',
   microsoft: {
     computerVision: {
       url: process.env.MS_CV_API_URL || 'https://westus.api.cognitive.microsoft.com/vision/v1.0/ocr',
