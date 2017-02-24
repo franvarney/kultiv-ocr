b7d060c8a15b891fc4f808e0c7b921c22a13a3bb
Fran Varney <fran.varn@gmail.com>
2017-02-24 09:24:21 -0500

    Add Microsoft Computer Vision API config

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  microsoft: {
+    computerVision: {
+      url: process.env.MS_CV_API_URL || 'https://westus.api.cognitive.microsoft.com/vision/v1.0/ocr',
+      key: process.env.MS_CV_API_KEY || 'key'
+    }
+  },
   port: process.env.PORT || '8000'
 };
